7dd043a1c335c31ecbc10362324a469647856fa2
Natalia Podorozhnaya <arkadiy.dryzhak@yandex.ru>
2022-09-01 22:34:04 +0300

    Added solution to problem 1

---
+++ task.js
@@ -0,0 +1,19 @@
+function compareArrays(arr1, arr2) {
+  let result;
+  
+  if(arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index])){
+    result = true;
+  } else {
+    result = false;
+  }
+  
+  return result; // boolean
+}
+
+function advancedFilter(arr) {
+  let resultArr;
+
+  // Ваш код
+
+  return resultArr; // array
+}
