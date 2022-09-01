ce5fa8ef4c3ef67d86fa79fc619f44fbc35b0e70
Natalia Podorozhnaya <arkadiy.dryzhak@yandex.ru>
2022-09-01 22:43:34 +0300

    Added solution to problem 2

--- task.js
+++ task.js
@@ -13,7 +13,7 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter(item => item >=0).filter(item => item % 3 === 0).map((item) => item * 10);
 
   return resultArr; // array
 }
